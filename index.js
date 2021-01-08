64df831e9b6365bfdeea72f4005a8f46822c2ffe
rimahour94 <59297120+rimahour94@users.noreply.github.com>
2021-01-08 15:36:49 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const http=require('http')
-const server=http.createServer((req,res)=>{
-      console.log(req.headers)
-      res.setHeader('Content-Type',"text/html")
-      res.end("Successfully started")
-})
-server.listen(3000)
